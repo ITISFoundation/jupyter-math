712afcd18e5d4e27abcf3db0f4255a68f8385698
Andrei Neagu <neagu@itis.swiss>
2024-03-07 14:12:29 +0100

    refactor

--- docker/kernel_checker.py
+++ docker/kernel_checker.py
@@ -12,18 +12,19 @@ from typing import Final
 
 KERNEL_BUSY_CHECK_INTERVAL_S: Final[float] = 5
 
+
 class JupyterKernelChecker:
     BASE_URL = "http://localhost:8888"
     HEADERS = {"accept": "application/json"}
 
     def __init__(self) -> None:
-        self.last_idle: datetime| None  = None
-    
+        self.last_idle: datetime | None = None
+
     def _get(self, path: str) -> dict:
-        r = requests.get(f'{self.BASE_URL}{path}', headers=self.HEADERS)
+        r = requests.get(f"{self.BASE_URL}{path}", headers=self.HEADERS)
         return r.json()
 
-    def _are_kernels_busy(self)-> bool:
+    def _are_kernels_busy(self) -> bool:
         json_response = self._get("/api/kernels")
 
         are_kernels_busy = False
@@ -36,29 +37,27 @@ class JupyterKernelChecker:
                 are_kernels_busy = True
 
         return are_kernels_busy
-    
+
     def check(self):
         are_kernels_busy = self._are_kernels_busy()
-        
+
         if are_kernels_busy:
             self.last_idle = None
 
         if not are_kernels_busy and self.last_idle is None:
             self.last_idle = datetime.utcnow()
 
-    
-    def get_idle_seconds(self)-> float:
+    def get_idle_seconds(self) -> float:
         if self.last_idle is None:
             return 0
 
         return (datetime.utcnow() - self.last_idle).total_seconds()
-    
+
     async def run(self):
         while True:
             with suppress(Exception):
                 self.check()
             await asyncio.sleep(KERNEL_BUSY_CHECK_INTERVAL_S)
-        
 
 
 kernel_checker = JupyterKernelChecker()
@@ -67,20 +66,20 @@ kernel_checker = JupyterKernelChecker()
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         idle_seconds = kernel_checker.get_idle_seconds()
-        response = (
-            {"seconds_inactive" : idle_seconds if idle_seconds > 0 else None} 
-        )
+        response = {"seconds_inactive": idle_seconds if idle_seconds > 0 else 0}
         self.write(json.dumps(response))
 
 
-def make_app()-> tornado.web.Application:
+def make_app() -> tornado.web.Application:
     return tornado.web.Application([(r"/", MainHandler)])
+
 
 async def main():
     app = make_app()
     app.listen(19597)
     asyncio.create_task(kernel_checker.run())
     await asyncio.Event().wait()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
