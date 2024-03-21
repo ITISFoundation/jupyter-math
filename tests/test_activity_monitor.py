a6d8c9b399ebf5ab29b2ea128b4550daac798e55
Andrei Neagu <neagu@itis.swiss>
2024-03-21 09:50:33 +0100

    cpu usage test

--- tests/test_activity_monitor.py
+++ tests/test_activity_monitor.py
@@ -9,7 +9,7 @@ from concurrent.futures import ThreadPoolExecutor, wait
 from multiprocessing import Array, Process
 from tempfile import NamedTemporaryFile
 
-from typing import Callable, Final, TYPE_CHECKING
+from typing import Callable, Final, TYPE_CHECKING, Iterable
 from pytest_mock import MockFixture
 from tenacity import AsyncRetrying
 from tenacity.stop import stop_after_delay
@@ -35,16 +35,19 @@ class _ListenSocketServer:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind(("localhost", _LOCAL_LISTEN_PORT))
         self.server_socket.listen(100)  # max number of connections
-        self.stop_event = threading.Event()
+        self._process: Process | None = None
 
     def start(self):
-        threading.Thread(target=self._accept_clients, daemon=True).start()
+        self._process = Process(target=self._accept_clients, daemon=True)
+        self._process.start()
 
     def stop(self):
-        self.stop_event.set()
+        if self._process:
+            self._process.terminate()
+            self._process.join()
 
     def _accept_clients(self):
-        while not self.stop_event.is_set():
+        while True:
             client_socket, _ = self.server_socket.accept()
             threading.Thread(
                 target=self._handle_client, daemon=True, args=(client_socket,)
@@ -52,7 +55,7 @@ class _ListenSocketServer:
 
     def _handle_client(self, client_socket):
         try:
-            while not self.stop_event.is_set():
+            while True:
                 data = client_socket.recv(1024)
                 if not data:
                     break
@@ -98,13 +101,13 @@ class _ActivityGenerator:
                     futures.append(executor.submit(self.__load_disk))
 
                 wait(futures)
-                time.sleep(0.01)
+                time.sleep(0.1)
 
     def __enter__(self):
         self.start()
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
 
     def start(self) -> None:
@@ -112,7 +115,8 @@ class _ActivityGenerator:
         self._process.start()
 
     def stop(self) -> None:
-        self.shared_array[3] = False
+        _keep_running = False
+        self.shared_array[3] = _keep_running
         if self._process:
             self._process.join()
 
@@ -130,30 +134,68 @@ def socket_server() -> None:
 
 
 @pytest.fixture
-def mock__get_brother_processes(mocker: MockFixture) -> Callable:
+def mock__get_brother_processes(mocker: MockFixture) -> Callable[[list[int]], list[psutil.Process]]:
+    def _get_processes(pids: list[int]) -> list[psutil.Process]:
+        results = []
+        for pid in pids:
+            proc = psutil.Process(pid)
+            assert proc.status()
+            results.append(proc)
+        return results
+
     def _(pids: list[int]) -> None:
         mocker.patch(
-            "activity_monitor._get_brother_processes",
-            return_value=[psutil.Process(p) for p in pids],
+            "activity_monitor._get_brother_processes", return_value=_get_processes(pids)
         )
 
     return _
 
 
-async def test_is_working(socket_server: None, mock__get_brother_processes: Callable):
-    with _ActivityGenerator(network=False, cpu=False, disk=False) as activity_generator:
-        mock__get_brother_processes([activity_generator.get_pid()])
+@pytest.fixture
+def create_activity_generator() -> (
+    Iterable[Callable[[bool, bool, bool], _ActivityGenerator]]
+):
+    created: list[_ActivityGenerator] = []
 
-        assert len(activity_monitor._get_brother_processes()) == 1
+    def _(*, network: bool, cpu: bool, disk: bool) -> _ActivityGenerator:
+        instance = _ActivityGenerator(network=network, cpu=cpu, disk=disk)
+        instance.start()
+        created.append(instance)
+        return instance
 
-        # some tests
-        with activity_monitor.CPUUsageMonitor(1, threshold=0) as cpu_usage_monitor:
-            # poll for it to be idle since it takes some time
-            async for attempt in AsyncRetrying(
-                stop=stop_after_delay(3), wait=wait_fixed(0.1), reraise=True
-            ):
-                with attempt:
-                    # TODO: figure out why test is wrong here
-                    assert cpu_usage_monitor.is_busy is False
+    yield _
 
-        # now we can test whatever here
+    for instance in created:
+        instance.stop()
+
+
+async def test_cpu_usage_monitor_not_busy(
+    socket_server: None,
+    mock__get_brother_processes: Callable[[list[int]], list[psutil.Process]],
+    create_activity_generator: Callable[[bool, bool, bool], _ActivityGenerator],
+):
+    activity_generator = create_activity_generator(network=False, cpu=False, disk=False)
+    mock__get_brother_processes([activity_generator.get_pid()])
+
+    with activity_monitor.CPUUsageMonitor(1, threshold=5) as cpu_usage_monitor:
+        async for attempt in AsyncRetrying(
+            stop=stop_after_delay(5), wait=wait_fixed(0.1), reraise=True
+        ):
+            with attempt:
+                assert cpu_usage_monitor.is_busy is False
+
+
+async def test_cpu_usage_monitor_still_busy(
+    socket_server: None,
+    mock__get_brother_processes: Callable[[list[int]], list[psutil.Process]],
+    create_activity_generator: Callable[[bool, bool, bool], _ActivityGenerator],
+):
+    activity_generator = create_activity_generator(network=False, cpu=True, disk=False)
+    mock__get_brother_processes([activity_generator.get_pid()])
+
+    with activity_monitor.CPUUsageMonitor(0.5, threshold=5) as cpu_usage_monitor:
+        # wait for monitor to trigger
+        time.sleep(1)
+
+        # must still result busy
+        assert cpu_usage_monitor.is_busy is True
